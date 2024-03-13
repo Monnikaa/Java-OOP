2950f05fea7543760edf22f715964c431e6da16f
Monika <monika.blagoeva77@gmail.com>
2024-03-13 15:49:39 +0000

    JOOP fc

---
+++ src/_2ExercisesWorkingWithAbstraction/_4TrafficLights/Main.java
@@ -0,0 +1,13 @@
+package _2ExercisesWorkingWithAbstraction._4TrafficLights;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //String[] input = Arrays.stream(sc.nextLine().split("\\s+"));
+    }
+}
